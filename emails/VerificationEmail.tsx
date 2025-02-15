1fb0d0c6373b9749b26df0c304e7444905ced621
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2025-02-15 08:17:04 +0530

    Update VerificationEmail.tsx

--- emails/VerificationEmail.tsx
+++ emails/VerificationEmail.tsx
@@ -59,7 +59,7 @@ export default function VerificationEmail({
 
           {/* Verify Button */}
           <Button
-            href={`http://localhost:3000/verify/${username}`}
+            href={`honest-review.vercel.app/verify/${username}`}
             className="mt-6 bg-teal-600 text-white px-6 py-3 rounded-lg text-sm font-medium hover:bg-teal-700 transition-all shadow-md inline-block"
           >
             ✅ Verify My Account
